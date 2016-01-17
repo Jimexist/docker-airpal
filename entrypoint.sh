8ca107bc65c34e49ef04e9b6fa0837e1e4d35136
Jiayu Liu <jimexist@gmail.com>
2016-01-17 18:40:43 +0800

    add presto user and presto catalog

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ PRESTO_COORDINATOR_URL=http://$PRESTO_PORT_8080_TCP_ADDR:$PRESTO_PORT_8080_TCP_P
 MYSQL_URL=jdbc:mysql://$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT/$MYSQL_DB
 
 java -Ddw.prestoCoordinator=$PRESTO_COORDINATOR_URL \
+     -Ddw.prestoCatalog=$PRESTO_CATALOG \
+     -Ddw.prestoUser=$PRESTO_USER \
      -Ddw.s3AccessKey=$S3_ACCESS_KEY \
      -Ddw.s3SecretKey=$S3_SECRET_KEY \
      -Ddw.s3Bucket=airpal \
